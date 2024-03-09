49f0eaa29fa0c5c0555b0caffaffd58eac180061
Maurício Szabo <mauricio.szabo@gmail.com>
2024-03-09 17:55:18 -0300

    Watch, and debug fixes

--- lib/nrepl/connection.rb
+++ lib/nrepl/connection.rb
@@ -7,6 +7,7 @@ module NREPL
       @in = input
       @out = out
       @pending_evals = {}
+      @watches = {}
       @counter = 0
     end
 
@@ -15,7 +16,7 @@ module NREPL
       loop do
         break if @in.eof?
         msg = bencode.parse!
-        puts "Received: #{msg.inspect}" if @debug
+        debug "Received", msg
         next unless msg
 
         # if @stopped
@@ -42,6 +43,15 @@ module NREPL
         msg['id'] ||= "eval_#{@counter += 1}"
         stop_id = msg['stop_id']
         clear_eval!(stop_id)
+
+        send_msg(response_for(msg, {
+          'status' => ['done'],
+          'op' => msg['op']
+        }))
+      when 'unwatch'
+        msg['id'] ||= "eval_#{@counter += 1}"
+        watch_id = msg['watch_id']
+        @watches.delete(watch_id)
 
         send_msg(response_for(msg, {
           'status' => ['done'],
@@ -99,9 +109,9 @@ module NREPL
     end
 
     private def clear_eval!(id)
-      stop_function_name = @pending_evals.fetch(id, {})[:stop_function_name]
-      if stop_function_name
-        NREPL.singleton_class.send(:undef_method, stop_function_name)
+      function_name = @pending_evals.fetch(id, {})[:stop_function_name]
+      if function_name
+        NREPL.singleton_class.send(:undef_method, "stop_#{function_name}")
       end
 
       input = @pending_evals.fetch(id, {})[:in]
@@ -114,28 +124,30 @@ module NREPL
       msg.merge('session' => old_msg.fetch('session', 'none'), 'id' => old_msg.fetch('id', 'unknown'))
     end
 
-    private def send_msg(msg)
-      puts "Sending: #{msg.inspect}" if @debug
+    def send_msg(msg)
+      debug "Sending", msg
       @out.write(msg.bencode)
       @out.flush
     end
 
     private def eval_msg(msg, stop)
-      puts "Eval: #{msg.inspect}" if @debug
-
       str   = msg['code']
       code  = str == 'nil' ? nil : str
       pending_eval = @pending_evals[msg['id']]
       value = unless code.nil?
         if stop
           @@debug_counter += 1
-          method_name = "stop_#{@@debug_counter}_#{rand(9999999999).to_s(32)}"
+          method_name = "_#{@@debug_counter}_#{rand(9999999999).to_s(32)}"
           pending_eval[:stop_function_name] = method_name
-          code = code.sub(/^NREPL\.stop!$/, "NREPL.#{method_name}(binding)")
+          code = code.gsub(/^NREPL\.(stop|watch)!$/, "NREPL.\\1_#{method_name}(binding)")
           define_stop_function!(msg, method_name)
         end
 
-        original_bind = @pending_evals.fetch(msg['stop_id'], {})[:binding] if msg['stop_id']
+        original_bind = if msg['stop_id']
+          @pending_evals.fetch(msg['stop_id'], {})[:binding]
+        elsif msg['watch_id']
+          @watches.fetch(msg['watch_id'], {})[:binding]
+        end
         evaluate_code(code, msg['file'], msg['line'], original_bind)
       end
 
@@ -146,7 +158,7 @@ module NREPL
 
     private def define_stop_function!(msg, method_name)
       out, inp = IO.pipe
-      send_stopped = proc do |ctx_binding, original_msg|
+      send_stopped = proc do |ctx_binding, original_msg, caller|
         stop_msg = @pending_evals[msg['id']]
         if stop_msg
           stop_msg.update(
@@ -155,7 +167,12 @@ module NREPL
           )
         end
         @pending_evals[original_msg['id']].update(stopped?: true)
-        send_msg(response_for(original_msg, {'status' => ['done', 'paused']}))
+        (file, row) = caller[0].split(/:/)
+        send_msg(response_for(original_msg, {
+          'file' => file,
+          'line' => row.to_i,
+          'status' => ['done', 'paused']
+        }))
       end
 
       will_pause = proc do
@@ -166,21 +183,39 @@ module NREPL
         stop_id = original_msg['stop_id']
         original_msg if stop_id == msg['id']
       end
-
-      NREPL.singleton_class.send(:define_method, method_name) do |ctx_binding|
+      NREPL.singleton_class.send(:define_method, "stop_#{method_name}") do |ctx_binding|
         original_msg = will_pause.call
         if original_msg
-          send_stopped.call(ctx_binding, original_msg)
-          out.read
+          send_stopped.call(ctx_binding, original_msg, caller)
         end
+      end
+
+      send_watched = proc do |ctx_binding, caller|
+        (file, row) = caller[0].split(/:/)
+        id = "watch_#{@counter += 1}"
+        @watches[id] = {binding: ctx_binding}
+        send_msg(
+          'op' => 'hit_watch',
+          'id' => id,
+          'file' => file,
+          'line' => row.to_i,
+          'status' => ['done']
+        )
+      end
+      NREPL.singleton_class.send(:define_method, "watch_#{method_name}") do |ctx_binding|
+        send_watched.call(ctx_binding, caller)
       end
     end
 
     private def register_session(msg)
-      puts "Register session: #{msg.inspect}" if @debug
+      debug "Register session", msg
 
       id = rand(4294967087).to_s(16)
       send_msg(response_for(msg, { 'new_session' => id, 'status' => ['done'] }))
+    end
+
+    private def debug(text, msg)
+      STDOUT.write("#{text}: #{msg.inspect}\n") if @debug
     end
 
     private def describe_msg(msg)
